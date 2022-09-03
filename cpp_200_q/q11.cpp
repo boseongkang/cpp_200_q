b900e61b64a35e8288975459da0e9914ece09cc9
boseongkang <okboseong@gmail.com>
2022-09-02 21:02:31 -0700

    function

---
+++ cpp_200_q/q11.cpp
@@ -0,0 +1,29 @@
+//
+//  q11.cpp
+//  cpp_200_q
+//
+//  Created by Boseong on 9/2/22.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void Minus(const int x, const int y)
+{
+    cout << "x - y = " << x - y << endl;
+}
+
+int Plus(const int x, const int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    Minus(10,5);
+    cout << "x + y = " << Plus(2,6) << endl;
+    
+    return 0;
+}
