efeb86fd1b6da6d5826a9970cb2d14dfc4ebdaef
boseongkang <okboseong@gmail.com>
2022-09-02 20:51:58 -0700

    Initial Commit

---
+++ cpp_200_q/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_200_q
+//
+//  Created by Boseong on 9/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
