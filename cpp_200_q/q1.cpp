042613f6eb4aef7aa7b8be8e567b1cd3868bcd52
boseongkang <okboseong@gmail.com>
2022-09-02 20:53:45 -0700

    hello world

--- cpp_200_q/q1.cpp
+++ cpp_200_q/q1.cpp
@@ -6,9 +6,11 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
+using namespace std;
+//int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    cout << "Hello C++ World!" << endl;
     return 0;
 }
