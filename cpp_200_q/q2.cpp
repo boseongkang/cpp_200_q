6011394e5c2f35bcc0126bd986fe763d470cec4d
boseongkang <okboseong@gmail.com>
2022-09-02 20:55:28 -0700

    cin

---
+++ cpp_200_q/q2.cpp
@@ -0,0 +1,21 @@
+//
+//  q2.cpp
+//  cpp_200_q
+//
+//  Created by Boseong on 9/2/22.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    
+    cin >> number;
+    
+    cout << "입력한 숫자는 " << number << "입니다." << endl;
+    
+    return 0;
+}
