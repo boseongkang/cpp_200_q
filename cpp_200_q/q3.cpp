91e7e4ad334e179a1ecd2de0e34e5dd102bf37e3
boseongkang <okboseong@gmail.com>
2022-09-02 20:56:57 -0700

    var_add

---
+++ cpp_200_q/q3.cpp
@@ -0,0 +1,21 @@
+//
+//  q3.cpp
+//  cpp_200_q
+//
+//  Created by Boseong on 9/2/22.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int one = 1;
+    int two = 2;
+    int sum = one + two;
+    
+    cout << "1 + 2 = " << sum << endl;
+    
+    return 0;
+}
